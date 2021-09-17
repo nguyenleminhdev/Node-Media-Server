a8f092cee5c36f835bea3cb6f21987732a80f560
Thien Minh <nguyenleminhdev@gmail.com>
2021-09-17 15:48:54 +0700

    add log rtmpChunkRead

--- src/node_rtmp_session.js
+++ src/node_rtmp_session.js
@@ -397,6 +397,8 @@ class NodeRtmpSession {
    */
   rtmpChunkRead(data, p, bytes) {
     // Logger.log('rtmpChunkRead', p, bytes);
+    console.log(bytes)
+    context.nodeEvent.emit('rtmpChunkRead', bytes);
     let size = 0;
     let offset = 0;
     let extended_timestamp = 0;
